46ded76f58a2518361d6b07b32ca4c8ac41e104c
illlilililiililll <kcwrenew22c@gmail.com>
2024-04-03 19:58:20 +0900

    Add Day 1 Description

--- 1. Variables and Operators/Day 1/HW1.java
+++ 1. Variables and Operators/Day 1/HW1.java
@@ -1,3 +1,8 @@
+// 변수 2개와 출력문 1줄을 이용해서 다음과 같은 결과를 보여주는 코드를 완성하기
+// EX. 넓이는 200입니다. 
+
+// 너비와 높이를 정의하여 사각형의 넓이를 계산
+
 public class HW1 {
     public static void main(String[] args) {
         int width = 10;
